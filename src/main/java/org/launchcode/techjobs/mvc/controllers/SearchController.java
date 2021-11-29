01df8ac0c67cc573a75aec65e287df89326a2b8f
Peach Paulison <dlpeachpaulison@gmail.com>
2021-11-29 16:41:17 -0500

    Finished through TODO #4.  Commit before testing.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -22,5 +28,28 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        if (searchTerm.equals("all")) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
+       return "search";
+    }
+    //@PostMapping(value="")
+    //PostMapping because the form in the search.html says "post" value="" because RequestMapping("search")
+   // public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+      //  ArrayList<Job> jobs;
 
-}
+//        if  the searchTerm is equal to All Jobs then jobs = JobData.findAll()...
+//           then add  model.addAttribute("title", "All Jobs");
+//         else
+//            jobs = JobData.by the searchType at least and searchTerm if available?
+//            model.addAttribute("title", "Jobs with " + columnChoices.whatever
+//            the searchType + ": " + searchTerm value if present?);
+       }
+
